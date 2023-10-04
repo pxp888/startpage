035b00ca3cf52d97cbc29f044b5c0d6b9f17b9e4
paul xavier perrine <pxperrine@gmail.com>
2023-10-05 01:47:09 +0200

    add dynamic header text color

---
+++ assets/js/dynamic-header.js
@@ -0,0 +1,99 @@
+// This script is used to dynamically alter the color of the header
+// text based on the background color of the body.  
+
+// this changes the tags of the header elements so the custom css
+// below can be applied. 
+function setHeaderTags() {
+    const header = document.getElementById("header");
+    header.setAttribute("id", "dheader");
+}
+
+// this adds the custom css to the header
+function setHeaderCSS() {
+    const style = document.createElement('style');
+    style.id = "headerStyleTag";
+    document.head.appendChild(style);
+    style.textContent = `
+    #dheader {
+        color: white;
+        opacity: 0.4;
+    
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: baseline;
+        height: min-content;
+        margin: 0rem;
+    }
+    
+    #dheader:hover {
+        animation: 0.5s opacityFadeIn;
+        animation-fill-mode: forwards;
+    }
+    
+    #dheader > div {
+        display: flex;
+        flex-direction: row;
+        align-items: baseline;
+    }
+    
+    #dheader .giant {
+        font-size: 4rem;
+        margin: 0rem;
+        color: gainsboro;
+    }
+    
+    #dheader p {
+        color: white;
+    }
+
+    #dheader a {
+        text-decoration: none;
+    }
+    
+    #dhelplink {
+        margin-right: 2rem;
+    }
+    
+    #dhelplink:hover {
+        color: black;
+        background-color: gainsboro;
+        cursor: pointer;
+    }
+    `;
+}
+
+// this is a helper function that changes the css of the header
+function headerCssChange(selector, property, value){
+    let sheet = document.getElementById("headerStyleTag").sheet;
+    let rules=sheet.cssRules;
+    let x;
+    for (let i=0; i<rules.length; i++) {
+        if (rules[i].selectorText == selector) { x = rules[i].style; }
+    }
+    x.setProperty(property, value);
+}
+
+// this alters the color of the header text based on the background color of the body
+function updateHeaderColor(color) {
+    let bodyBackgroundColor = window.getComputedStyle(document.body).getPropertyValue('background-color');
+    let bodyBackgroundColorAverage = bodyBackgroundColor.match(/\d+/g).reduce((a,b) => parseInt(a) + parseInt(b), 0) / 3;
+    if (bodyBackgroundColorAverage < 128) {
+        headerCssChange("#dheader .giant", "color", "white");
+        headerCssChange("#dheader p", "color", "white");
+        
+    } else {
+        headerCssChange("#dheader .giant", "color", "black");
+        headerCssChange("#dheader p", "color", "black");
+    }
+}
+
+// setup functions
+setHeaderTags();
+setHeaderCSS();
+updateHeaderColor();
+
+// event listeners
+document.getElementById("backgroundColorPicker").addEventListener("input", updateHeaderColor);
+
+
