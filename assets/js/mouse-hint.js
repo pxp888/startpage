04486a5ea46ea40f428f20e970e0732cde922a57
paul xavier perrine <pxperrine@gmail.com>
2023-10-14 23:06:01 +0200

    add mouse-hint css file

--- assets/js/mouse-hint.js
+++ assets/js/mouse-hint.js
@@ -4,30 +4,23 @@
 
 
 // this is the mouse hint element that appears when you hover over the input fields
-let hint = document.createElement('div');
+const hint = document.createElement('div');
 
 // sets up the hint element
 function setupMouseHint() {
     hint.id = 'mousehint';
+    hint.className = 'mousehint';
     hint.textContent = 'Mouse hint';
     hint.style.position = 'absolute';
     hint.style.top = '0';
     hint.style.left = '0';
-    hint.style.width = 'auto';
-    hint.style.height = 'auto';
-    hint.style.padding = '5px';
-    hint.style.borderRadius = '5px';
-    hint.style.backgroundColor = 'white';
-    hint.style.color = 'black';
-    hint.style.opacity = 0;
-    hint.style.transition = 'opacity 0.3s ease-in-out';
     settingScreen.appendChild(hint);
 }
 
 // Show the hint element over the Icon URL input field
 function showIconMouseHint(event) {
     hint.textContent = 'Hint: You can drag images from other websites to set the Icon URL.';
-    hint.style.opacity = 1;
+    hint.classList.add('active');
     hint.style.left = event.clientX + 30 + 'px';
     const rect = settingScreen.getBoundingClientRect();
     hint.style.top = event.clientY - rect.top + 'px';
@@ -36,7 +29,7 @@ function showIconMouseHint(event) {
 // Show the hint element over the Shortcut Name input field
 function showNameMouseHint(event) {
     hint.textContent = 'This is whatever you want to call your shortcut.';
-    hint.style.opacity = 1;
+    hint.classList.add('active');
     hint.style.left = event.clientX + 30 + 'px';
     const rect = settingScreen.getBoundingClientRect();
     hint.style.top = event.clientY - rect.top + 'px';
@@ -45,7 +38,8 @@ function showNameMouseHint(event) {
 // Show the hint element over the Shortcut URL input field
 function showUrlMouseHint(event) {
     hint.textContent = 'This is the URL that the shortcut will open.';
-    hint.style.opacity = 1;
+
+    hint.classList.add('active');
     hint.style.left = event.clientX + 30 + 'px';
     const rect = settingScreen.getBoundingClientRect();
     hint.style.top = event.clientY - rect.top + 'px';
@@ -53,7 +47,7 @@ function showUrlMouseHint(event) {
 
 // Hide the hint element
 function hideMouseHint(event) {
-    hint.style.opacity = 0;
+    hint.classList.remove('active');
 }
 
 setupMouseHint();
