c96f0d51d27b9b618d187e13f3737e071f3be261
pxp888 <pxperrine@gmail.com>
2023-11-27 14:31:49 +0100

    add background image dropzone

--- assets/js/drag-drop.js
+++ assets/js/drag-drop.js
@@ -4,22 +4,55 @@
 
 const imageDropZone = document.createElement("div");
 const linkDropZone = document.createElement("div");
+const backDropZone = document.createElement("div");
+
+
 
 // set up the drop target
 function setupDropZone() {
 
-    imageDropZone.id = "imageDropZone";
-    imageDropZone.className = "dropZone";
-    imageDropZone.innerHTML = "Drop image here to set Icon URL";
-    imageDropZone.style.top = "45%";
-    settingScreen.appendChild(imageDropZone);
-
     linkDropZone.id = "linkDropZone";
     linkDropZone.className = "dropZone";
     linkDropZone.innerHTML = "Drop link here to set URL";
-    linkDropZone.style.top = "15%";
+    linkDropZone.style.top = "10%";
     settingScreen.appendChild(linkDropZone);
+
+    imageDropZone.id = "imageDropZone";
+    imageDropZone.className = "dropZone";
+    imageDropZone.innerHTML = "Drop image here to set Icon URL";
+    imageDropZone.style.top = "35%";
+    settingScreen.appendChild(imageDropZone);
+
+    backDropZone.id = "backDropZone";
+    backDropZone.className = "dropZone";
+    backDropZone.innerHTML = "Drop link here to set background Image";
+    backDropZone.style.top = "60%";
+    settingScreen.appendChild(backDropZone);
 }
+
+
+// set up the background image
+function setupBackImage() {
+    const backImage = document.createElement("div");
+    backImage.id = "backImage";
+    backImage.style.backgroundImage = "url('')";
+    backImage.style.backgroundSize = "cover";
+    backImage.style.backgroundPosition = "center";
+    backImage.style.backgroundRepeat = "no-repeat";
+    backImage.style.width = "100%";
+    backImage.style.height = "100%";
+    backImage.style.position = "fixed";
+    backImage.style.top = "0";
+    backImage.style.left = "0";
+    backImage.style.zIndex = "-1";
+    document.body.appendChild(backImage);
+
+    const backImageAddress = localStorage.getItem("backImage");
+    if (backImageAddress != null) {
+        backImage.style.backgroundImage = "url('" + backImageAddress + "')";
+    }
+}
+
 
 // this is necessary for drag and drop to work
 function handleDragOver(event)
@@ -28,7 +61,9 @@ function handleDragOver(event)
     event.preventDefault();
     imageDropZone.classList.add("active");
     linkDropZone.classList.add("active");
+    backDropZone.classList.add("active");
 }
+
 
 // set icon URL from dropped images
 function imageDrop(event) {
@@ -45,6 +80,7 @@ function imageDrop(event) {
     shortcutIcon.value = imageAddress;
     iconFieldUpdate({target: shortcutIcon});
 }
+
 
 // set URL field from dropped links
 function linkDrop(event) {
@@ -66,6 +102,31 @@ function linkDrop(event) {
     }
 }
 
+
+// set background image
+function backDrop(event) {
+    event.preventDefault();
+
+    const imageUrl = event.dataTransfer.getData("text/html");
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(imageUrl, "text/html");
+    const img = doc.querySelector("img");
+    if (img==null) {
+        return; }
+    const imageAddress = img.src;
+    backImage.style.backgroundImage = "url('" + imageAddress + "')";
+
+    localStorage.setItem("backImage", imageAddress);
+}
+
+
+// clear the background Image
+function clearBackImage() {
+    backImage.style.backgroundImage = "url('')";
+    localStorage.removeItem("backImage");
+}
+
+
 function isValidURL(url) {
     try {
         new URL(url);
@@ -76,16 +137,20 @@ function isValidURL(url) {
 }
 
 
-
 function hideDropZone(event) {
     imageDropZone.classList.remove("active");
     linkDropZone.classList.remove("active");
+    backDropZone.classList.remove("active");
 }
 
+
 setupDropZone();
+setupBackImage();
 
 imageDropZone.addEventListener("drop", imageDrop);
 linkDropZone.addEventListener("drop", linkDrop);
+backDropZone.addEventListener("drop", backDrop);
 document.addEventListener("dragover", handleDragOver);
 document.addEventListener("mouseout", hideDropZone);
 document.addEventListener("drop", hideDropZone);
+document.getElementById("noBackButton").addEventListener("click", clearBackImage);
